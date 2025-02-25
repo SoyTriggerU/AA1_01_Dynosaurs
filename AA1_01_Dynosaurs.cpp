344ba73036cc1a066bd10d7184e4ac66c74be9ae
SoyTriggerU <unai.gonzalez@enti.cat>
2025-02-25 10:54:34 +0100

    Base Functions Created

--- AA1_01_Dynosaurs.cpp
+++ AA1_01_Dynosaurs.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+#define NUM_DYNOSAURS = 2
+
+enum DynosaurType { TYRANNOSAURUS, VELOCIRAPTOR, BRACHIOSAURUS, DIPLODOCUS };
+
+struct Dynosaur
+{
+    DynosaurType dynosaur;
+    std::string dynosaurName;
+    int health = 100;
+    int attackPower;
+};
+
+int getDynosaurTypeToString(DynosaurType dynosaur, std::string dynosaurName)
+{
+     
+}
+
+int compareDynosaurStrength(Dynosaur strength)
+{
+
+}
+
+int createRandomDynosaur()
+{
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
